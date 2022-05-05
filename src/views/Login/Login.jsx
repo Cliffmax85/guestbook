312d1916668084cae627ac403db1288f36b607fc
cliffmax85 <cliffmax85@gmail.com>
2022-05-05 12:51:50 -0700

    rendered a input on page

---
+++ src/views/Login/Login.jsx
@@ -0,0 +1,18 @@
+
+
+export default function Login() {
+    return(
+        <>
+          <form>
+              <label>
+                  <input
+                    id='email'
+                    name='email'
+                    type='email'
+                    placeholder="Enter Email"
+                  />
+              </label>
+          </form>
+        </>
+    );
+}
